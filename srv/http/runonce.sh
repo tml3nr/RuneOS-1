93f88ac31e01ed273a77aeb9b0a32410aed6626a
rern <rernrern@gmail.com>
2019-10-18 12:34:18 +0700

    Update runonce.sh

--- srv/http/runonce.sh
+++ srv/http/runonce.sh
@@ -218,3 +218,6 @@ fi
 echo -e 'd\n\nn\n\n\n\n\nw' | fdisk /dev/mmcblk0 &>/dev/null
 partprobe /dev/mmcblk0
 resize2fs /dev/mmcblk0p2
+
+# fix dirty bit if any
+fsck.fat -trawl /dev/mmcblk0p1 &> /dev/null &
