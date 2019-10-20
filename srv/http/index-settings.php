44e43c205aa3ecaff391e5f7eeea25b5a015bede
rern <rernrern@gmail.com>
2019-10-20 20:13:17 +0700

    Update index-settings.php

--- srv/http/index-settings.php
+++ srv/http/index-settings.php
@@ -20,7 +20,7 @@
 	</style>
 	<link rel="stylesheet" href="/assets/css/selectric.<?=$time?>.css">
 	<link rel="stylesheet" href="/assets/css/info.<?=$time?>.css">
-	<link rel="stylesheet" href="/assets/css/indexsettings.<?=$time?>.css">
+	<link rel="stylesheet" href="/assets/css/settings.<?=$time?>.css">
 	<link rel="stylesheet" href="/assets/css/banner.<?=$time?>.css">
 	<link rel="icon" type="image/png" href="/img/favicon-192x192.<?=$time?>.png" sizes="192x192">
 </head>
