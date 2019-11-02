852ae3315dbf7114315cd19060e81fca15647946
rern <rernrern@gmail.com>
2019-11-02 09:27:06 +0700

    Update write-alarm.sh

--- usr/local/bin/write-alarm.sh
+++ usr/local/bin/write-alarm.sh
@@ -3,24 +3,31 @@
 [[ ! -e /usr/bin/bsdtar ]] && apt install -y bsdtar
 [[ ! -e /usr/bin/nmap ]] && apt install -y nmap
 
+cols=$( tput cols )
+hr() {
+    printf %"$cols"s | tr ' ' -
+}
 tcolor() {
     echo -e "  \e[38;5;6m$1\e[0m" "$2"
 }
-cols=$( tput cols )
-
-showData() {
-    printf %"$cols"s | tr ' ' -
-    [[ -n $3 ]] && echo -e "$1\n$2$3" || echo $2 Not found!
-    printf %"$cols"s | tr ' ' -
-}
-showROOT() {
-    ROOT=$( df | grep ROOT | awk '{print $NF}' )
-    showData "$( df -h | grep ROOT )" "ROOT: " "$ROOT"
-    [[ -z $ROOT ]] && echo Not mounted or incorrect label. && exit
+showpath() {
+    mountpoint=$( df | grep $1 | awk '{print $NF}' )
+	hr
+	if [[ -n $mountpoint ]]; then
+		echo $( df -h | grep $mountpoint )
+		echo $1: $mountpoint
+		hr
+	else
+		echo $1 not mounted or incorrect label. && exit
+		hr
+    fi
 }
 showBOOT() {
     BOOT=$( df | grep BOOT | awk '{print $NF}' )
-    showData "$( df -h | grep BOOT )" "BOOT: " "$BOOT"
+	hr
+    echo $( df -h | grep BOOT )
+	echo BOOT: $BOOT
+	hr
     [[ -z $BOOT ]] && echo Not mounted or incorrect label. && exit
 }
 selectRPi() {
@@ -34,15 +41,14 @@ selectRPi() {
     read -rn 1 -p "Select [0-5]: " rpi; echo
     [[ -z $rpi ]] || (( $rpi > 5 )) && echo -e "\nSelect 0, 1, 2, 3, 4 or 5\n" && selectRPi
 }
+# -----------------------------------------------------------------------
 
-selectRPi
-
-showBOOT
+showpath BOOT
 read -rn 1 -p "Confirm path - BOOT [y/N]: " ans; echo
 [[ $ans != y && $ans != Y ]] && exit
 [[ -n $( ls $BOOT ) ]] && echo $BOOT not empty. && exit
 
-showROOT
+showpath ROOT
 read -rn 1 -p "Confirm path - ROOT [y/N]: " ans; echo
 [[ $ans != y && $ans != Y ]] && exit
 [[ -n $( ls $ROOT | grep -v lost+found ) ]] && echo $ROOT not empty. && exit
@@ -52,6 +58,7 @@ tcolor 1 'Micro SD card'
 tcolor 2 'USB drive'
 read -rn 1 -p "Select [1/2]: " mode; echo
 
+selectRPi
 if [[ $rpi == 0 || $rpi == 1 ]]; then
 	file=ArchLinuxARM-rpi-latest.tar.gz
 elif [[ $rpi == 2 || $rpi == 3 ]]; then
@@ -61,6 +68,26 @@ elif [[ $rpi == 5 ]]; then
 elif [[ $rpi == 4 ]]; then
 	file=ArchLinuxARM-rpi-4-latest.tar.gz
 fi
+
+read -rn 1 -p "Setup Wi-Fi auto-connect [y/N]: " ans; echo
+if [[ $ans == y || $ans == Y ]]; then
+    selectSecurity() {
+        echo Security:
+        tcolor 1 'WPA'
+        tcolor 2 'WEP'
+        tcolor 3 'None'
+        read -rn 1 -p 'Select [1-3]: ' ans
+        [[ -z $ans ]] || (( $ans > 3 )) && echo -e "\nSelect 1, 2 or 3\n" && selectSecurity
+        if [[ $ans == 1 ]]; then
+            wpa=wpa
+        elif [[ $ans == 2 ]]; then
+            wpa=wep
+        else
+            wpa=
+        fi
+    }
+fi
+# -----------------------------------------------------------------------
 
 echo -e "\nDownloading ..."
 wget -qN --show-progress http://os.archlinuxarm.org/os/$file
@@ -79,6 +106,10 @@ if [[ $mode == 2 ]]; then
     sed -i "s|/dev/mmcblk0p2|$uuid|" $BOOT/cmdline.txt
     echo "$uuid  /  ext4  defaults  0  0" >> $ROOT/etc/fstab
 fi
+
+# get write.rune.sh
+wget -qN --show-progress https://github.com/rern/RuneOS/raw/master/usr/local/bin/write-rune.sh -P $ROOT/usr/local/bin
+chmod +x $ROOT/usr/local/bin/write-rune.sh
 
 # RPi 0 - fix: kernel panic
 [[ $rpi == 0 ]] && echo -e 'force_turbo=1\nover_voltage=2' >> $BOOT/config.txt
@@ -135,9 +166,5 @@ if [[ $ans == y || $ans == Y ]]; then
     ln -s ../../../../lib/systemd/system/netctl@.service "netctl@$ssid.service"
     cd
 fi
-
-# get write.rune.sh
-wget -qN --show-progress https://github.com/rern/RuneOS/raw/master/usr/local/bin/write-rune.sh -P $ROOT/usr/local/bin
-chmod +x $ROOT/usr/local/bin/write-rune.sh
 
 umount -l $BOOT && umount -l $ROOT && echo -e "\n$ROOT and $BOOT unmounted.\nMove to Raspberry Pi."
