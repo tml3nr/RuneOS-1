2e3144ed95d9d831cb5d38b32aa8da5e4f89784e
rern <rernrern@gmail.com>
2019-11-02 22:04:16 +0700

    Update create-rune.sh

--- usr/local/bin/create-rune.sh
+++ usr/local/bin/create-rune.sh
@@ -9,7 +9,7 @@ hr() {
 }
 
 hr
-echo -e "\n\n\e[36mRuneAudio+Re systm ...\e[m\n"
+echo -e "\n\n\e[36mCreate RuneAudio+Re ...\e[m\n"
 hr
 
 #---------------------------------------------------------------------------------
