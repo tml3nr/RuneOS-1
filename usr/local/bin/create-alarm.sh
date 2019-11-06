c0004e09d812855c5ea19b9fac813dc5f2714ee0
rern <rernrern@gmail.com>
2019-11-06 22:00:52 +0700

    Update create-alarm.sh

--- usr/local/bin/create-alarm.sh
+++ usr/local/bin/create-alarm.sh
@@ -19,7 +19,7 @@ trap 'rm -f ArchLinuxARM*; clear' EXIT
 #----------------------------------------------------------------------------
 title='Create Arch Linux Arm'
 dialog --colors \
-    --infobox "\n             \Z1$title\Z0\n" 5 50
+	--infobox "\n             \Z1$title\Z0\n" 5 50
 sleep 3
 
 BOOT=$( df | grep BOOT | awk '{print $NF}' )
@@ -55,12 +55,12 @@ ROOT: \Z1$ROOT\Z0\n\n" 0 0
 rpi=$( dialog --backtitle "$title" --colors \
 	--output-fd 1 \
 	--radiolist '\n\Z1Target:\Z0' 0 0 6 \
-        0 'Raspberry Pi Zero' off \
-        1 'Raspberry Pi 1' off \
-        2 'Raspberry Pi 2' off \
-        3 'Raspberry Pi 3' on \
-        4 'Raspberry Pi 4' off \
-        5 'Raspberry Pi 3+' off )
+		0 'Raspberry Pi Zero' off \
+		1 'Raspberry Pi 1' off \
+		2 'Raspberry Pi 2' off \
+		3 'Raspberry Pi 3' on \
+		4 'Raspberry Pi 4' off \
+		5 'Raspberry Pi 3+' off )
 [[ $? == 255 ]] && clear && exit
 
 if [[ $rpi == 0 || $rpi == 1 ]]; then
@@ -78,8 +78,8 @@ fi
 mode=$( dialog --backtitle "$title" --colors \
 	--output-fd 1 \
 	--radiolist "\n\Z1Run on:\Z0" 0 0 2 \
-        1 'Micro SD card' on \
-        2 'USB drive' off )
+		1 'Micro SD card' on \
+		2 'USB drive' off )
 [[ $? == 255 ]] && clear && exit
 [[ $mode == 1 ]] && dev='Micro SD card' || dev='USB drive'
 
@@ -91,27 +91,27 @@ ans=$?
 if [[ $ans == 0 ]]; then
 	ssid=$( dialog --backtitle "$title" --output-fd 1 \
 		--inputbox 'Wi-Fi - SSID:' 0 0 )
-    [[ $? == 255 ]] && clear && exit
+	[[ $? == 255 ]] && clear && exit
 
 	password=$( dialog --backtitle "$title" --output-fd 1 \
 		--inputbox 'Wi-Fi - Password:' 0 0 )
-    [[ $? == 255 ]] && clear && exit
+	[[ $? == 255 ]] && clear && exit
 		
 	wpa=$( dialog --backtitle "$title" --output-fd 1 \
 		--radiolist 'Wi-Fi -Security:' 0 0 3 \
 			1 'WPA' on \
 			2 'WEP' off \
 			3 'None' off )
-    [[ $? == 255 ]] && clear && exit
-    
-    if [[ $wpa == 1 ]]; then
+	[[ $? == 255 ]] && clear && exit
+	
+	if [[ $wpa == 1 ]]; then
 		wpa=wpa
 	elif [[ $wpa == 2 ]]; then
 		wpa=wep
 	else
 		wpa=
 	fi
-    wifi="Wi-Fi settings\n\
+	wifi="Wi-Fi settings\n\
  SSID     : \Z1$ssid\Z0\n\
  Password : \Z1$password\Z0\n\
  Security : \Z1${wpa^^}\Z0\n\n"
@@ -128,24 +128,24 @@ $wifi" 0 0
 
 # download
 wget http://os.archlinuxarm.org/os/$file 2>&1 | \
-    stdbuf -o0 awk '/[.] +[0-9][0-9]?[0-9]?%/ { print substr($0,63,3) }' | \
-    dialog --backtitle "$title" \
-        --gauge "Download Arch Linux Arm ..." 0 50
+	stdbuf -o0 awk '/[.] +[0-9][0-9]?[0-9]?%/ { print substr($0,63,3) }' | \
+	dialog --backtitle "$title" \
+		--gauge "Download Arch Linux Arm ..." 0 50
 wget http://os.archlinuxarm.org/os/$file.md5 2>&1 | \
-    stdbuf -o0 awk '/[.] +[0-9][0-9]?[0-9]?%/ { print substr($0,63,3) }' | \
-    dialog --backtitle "$title" \
-        --gauge "Download checksum ..." 0 50
+	stdbuf -o0 awk '/[.] +[0-9][0-9]?[0-9]?%/ { print substr($0,63,3) }' | \
+	dialog --backtitle "$title" \
+		--gauge "Download checksum ..." 0 50
 
 # expand
 ( pv -n $file | bsdtar -C $BOOT --strip-components=2 --no-same-permissions --no-same-owner -xf - boot ) 2>&1 | \
-    dialog --backtitle "$title" \
-        --gauge "Expand to BOOT ..." 0 50
+	dialog --backtitle "$title" \
+		--gauge "Expand to BOOT ..." 0 50
 ( pv -n $file | bsdtar -C $ROOT --exclude='boot' -xpf - ) 2>&1 | \
-    dialog --backtitle "$title" \
-        --gauge "Expand to ROOT ...\n" 0 50
+	dialog --backtitle "$title" \
+		--gauge "Expand to ROOT ...\n" 0 50
 
 dialog --backtitle "$title" --colors \
-    --infobox "\n\Z1Be patient.\Z0\nIt may takes 10+ minutes \nto complete writing SD card or thumb drive." 7 50
+	--infobox "\n\Z1Be patient.\Z0\nIt may takes 10+ minutes \nto complete writing SD card or thumb drive." 7 50
 sync
 
 #----------------------------------------------------------------------------
@@ -192,8 +192,8 @@ umount -l $BOOT
 umount -l $ROOT
 
 dialog --colors \
-    --msgbox "\n       Arch Linux Arm for \Z1Raspberry Pi $rpi\Z0\n\
-              created successfully.\n" 8 58
+	--msgbox "\n       Arch Linux Arm for \Z1Raspberry Pi $rpi\Z0\n\
+			  created successfully.\n" 8 58
 
 #----------------------------------------------------------------------------
 [[ $mode == 2 ]] && usb=' and USB drive'
@@ -212,19 +212,19 @@ ans=$?
 [[ $ans == 255 ]] && clear && exit
 
 if [[ $ans == 1 ]]; then
-    dialog --backtitle "$title" \
-        --infobox "\nScan IP address ..." 5 50
-    routerip=$( ip route get 1 | cut -d' ' -f3 )
-    nmap=$( nmap -sP ${routerip%.*}.* | grep -v 'Starting\|Host is up\|Nmap done' | head -n -1 | sed 's/$/\\n/; s/Nmap.*for/\\nIP  :/; s/Address//' | tr -d '\n' )
-    dialog --backtitle "$title" --colors \
-        --msgbox "\n\Z1Find IP address of Raspberry Pi:\Z0\n
+	dialog --backtitle "$title" \
+		--infobox "\nScan IP address ..." 5 50
+	routerip=$( ip route get 1 | cut -d' ' -f3 )
+	nmap=$( nmap -sP ${routerip%.*}.* | grep -v 'Starting\|Host is up\|Nmap done' | head -n -1 | sed 's/$/\\n/; s/Nmap.*for/\\nIP  :/; s/Address//' | tr -d '\n' )
+	dialog --backtitle "$title" --colors \
+		--msgbox "\n\Z1Find IP address of Raspberry Pi:\Z0\n
 (Raspberri Pi 4 may listed as Unknown)\n
 $nmap" 50 100
 fi
 
 # connect RPi
 rpiip=$( dialog --backtitle "$title" \
-    --inputbox 'Raspberry Pi IP:' 0 0 )
+	--inputbox 'Raspberry Pi IP:' 0 0 )
 [[ $? == 255 ]] && clear && exit
 
 clear
