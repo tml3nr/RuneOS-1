04ac571e0e2649962ea8e58179ea35115c3b6b06
rern <rernrern@gmail.com>
2019-11-07 20:14:45 +0700

    Update create-alarm.sh

--- usr/local/bin/create-alarm.sh
+++ usr/local/bin/create-alarm.sh
@@ -90,10 +90,10 @@ getData() {
 		[[ $? == 255 ]] && clear && exit
 
 		wpa=$( dialog --backtitle "$title" --output-fd 1 \
-			--radiolist '\nWi-Fi -Security:\n[space] = select' 0 0 3 \
-				1 'WPA' on \
-				2 'WEP' off \
-				3 'None' off )
+			--menu '\nWi-Fi -Security:\n' 0 0 3 \
+				1 'WPA' \
+				2 'WEP' \
+				3 'None' )
 		[[ $? == 255 ]] && clear && exit
 
 		if [[ $wpa == 1 ]]; then
