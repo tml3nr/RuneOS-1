f93a2af43d488c78c9950a55cd8ca1c0b7a199a0
rern <rernrern@gmail.com>
2019-11-08 16:49:16 +0700

    Update create-alarm.sh

--- usr/local/bin/create-alarm.sh
+++ usr/local/bin/create-alarm.sh
@@ -153,9 +153,9 @@ sync
 PATH=$PATH:/sbin  # Debian not include /sbin in PATH
 partuuidBOOT=$( blkid | grep $( df | grep BOOT | awk '{print $1}' ) | awk '{print $NF}' | tr -d '"' )
 partuuidROOT=$( blkid | grep $( df | grep ROOT | awk '{print $1}' ) | awk '{print $NF}' | tr -d '"' )
-sed -i "s|/dev/mmcblk0p2|$partuuidROOT|" $BOOT/cmdline.txt
 echo "$partuuidBOOT  /boot  vfat  defaults  0  0
 $partuuidROOT  /      ext4  defaults  0  0" > $ROOT/etc/fstab
+sed -i "s|/dev/mmcblk0p2|$partuuidROOT|" $BOOT/cmdline.txt
 
 # RPi 0 - fix: kernel panic
 [[ $rpi == Zero ]] && echo -e 'force_turbo=1\nover_voltage=2' >> $BOOT/config.txt
